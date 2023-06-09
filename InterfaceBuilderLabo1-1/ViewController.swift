d43167aa2179611d1c025e23eb25c4449746fd5b
Mathieu H <mathieuhatin@hotmail.com>
2023-06-08 21:32:57 -0700

    Initial Commit

---
+++ InterfaceBuilderLabo1-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InterfaceBuilderLabo1-1
+//
+//  Created by macos on 18/03/1402 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
