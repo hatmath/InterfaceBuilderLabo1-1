c19a5b5ab0f7861312127e81b970c30c35dae8eb
Mathieu H <mathieuhatin@hotmail.com>
2023-06-09 10:05:56 -0700

    mon interface prend forme ...

--- InterfaceBuilderLabo1-1/ViewController.swift
+++ InterfaceBuilderLabo1-1/ViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //-- Outlets
+    
+    @IBOutlet weak var txtOutput: UITextField!
+    @IBOutlet weak var sldUserGuess: UISlider!
+    
+    //-- Variables globales
+    
+    
+    //-- Fonctions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
